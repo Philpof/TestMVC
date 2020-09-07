d95114721304226dc057bc215d8939fdfe2daeb5
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-09-07 21:22:08 +0200

    Corrections mineures

--- view/templateHeader.php
+++ view/templateHeader.php
@@ -10,6 +10,13 @@
 
     <title><?php echo $title; ?></title>
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Fontawsome -->
+    <script src="https://kit.fontawesome.com/fbdd9c8340.js" crossorigin="anonymous"></script>
+
+    <!-- Style CSS -->
     <link rel="stylesheet" href="public/CSS/style.css">
 
   </head>
@@ -18,6 +25,12 @@
 
     <?php echo $content; ?>
 
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script type="text/javascript"src="public/JS/script.js"></script>
+
   </body>
 
-</html>
+  </html>
